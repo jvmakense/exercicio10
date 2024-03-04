2f29dacbf51a8a8aab41b4fbf54e5f05295940f7
joao vitor <joaomakenze@gmail.com>
2024-03-04 11:38:41 -0300

    exercício_jquery

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    // Adiciona uma nova tarefa
     $('#adicionarTarefa button').click(function() {
         let tarefa = $('#adicionarTarefa input').val();
         if(tarefa.trim() !== '') {
@@ -8,14 +7,12 @@ $(document).ready(function() {
         } 
     });
 
-    // Riscar a tarefa quando clicada
     $('#lista ul').on('click', 'li', function() {
         $(this).toggleClass('riscada');
     });
 
-    // Excluir tarefa quando o botão de exclusão é clicado
     $('#lista ul').on('click', 'li button.excluir', function(event) {
         $(this).parent().remove();
-        event.stopPropagation(); // Impede que o evento de clique do item seja acionado
+        event.stopPropagation();
     });
 });
